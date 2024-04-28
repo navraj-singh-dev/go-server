ee23d7ac1f31b6df43c7ae459cd5e8765ef68a9f
navraj-singh-dev <navrajsingh.dev@gmail.com>
2024-04-28 20:04:24 +0530

    add content to all static files and implement all handlers
    - Implemented all the routers completely
    - Added styled css and html content to all the static files.
    - Server is all setup and running.

--- main.go
+++ main.go
@@ -6,6 +6,58 @@ import (
 	"net/http"
 )
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	/*
+		Remember that this handler is called by the form.html itself.
+		From the browser you must open form.html and submit it
+		then this handler will run, otherise from browser it cannot
+		be run successfully, error will come if not called from
+		form.html
+	*/
+
+	// return error response if request came on wrong route
+	if r.URL.Path != "/form" {
+		http.Error(w, "Wrong route", http.StatusBadGateway)
+		return
+	}
+
+	// parse the form, well form came in the request must be parsed before getting data from it
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "Could not parse the form that was in the request:\n%v", err)
+		return
+	}
+
+	// Fill the response with success message and data
+	fmt.Fprintf(w, "POST request successfull\n\n")
+	// extract data from form
+	name := r.Form.Get("name")
+	email := r.Form.Get("email")
+	message := r.Form.Get("message")
+	// add extracted data to response
+	fmt.Fprintf(w, "Name = %v\n", name)
+	fmt.Fprintf(w, "Email = %v\n", email)
+	fmt.Fprintf(w, "Message = %v\n", message)
+
+}
+
+func greethandler(w http.ResponseWriter, r *http.Request) {
+	// return error if wrong endpoint
+	if r.URL.Path != "/greet" {
+		http.Error(w, "Wrong path, send request to /greetings", http.StatusBadRequest)
+		return
+	}
+
+	// return error if wrong HTTP method
+	if r.Method != "GET" {
+		http.Error(w, "Wrong method, Use GET", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// good, now serve the data in response
+	fmt.Fprintf(w, "Hello, You Made A Successful Request\nCongratulations!")
+
+}
+
 func main() {
 	// define the port to listen
 	PORT := ":8000"
